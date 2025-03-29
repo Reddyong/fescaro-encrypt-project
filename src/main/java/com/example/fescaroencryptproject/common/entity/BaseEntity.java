eedd9375ec9549429e21d25f9d4e47aafa4c28ad
Reddyong <d0209hong@naver.com>
2025-03-29 17:11:21 +0900

    initial commit
    #1 feat : BaseEntity 생성
    
    * 기본 공통 속성인 id, createdAt 속성에 대해 BaseEntity로 분류

---
+++ src/main/java/com/example/fescaroencryptproject/common/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.example.fescaroencryptproject.common.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
