bad17aaf72d1634303fdaa3a726d38bb9b351cbc
Reddyong <d0209hong@naver.com>
2025-03-29 16:34:07 +0900

    initial commit
    * 파일 암호화 개발 프로젝트 초기 세팅

---
+++ src/main/java/com/example/fescaroencryptproject/FescaroEncryptProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fescaroencryptproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FescaroEncryptProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FescaroEncryptProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fescaroencryptproject/FescaroEncryptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fescaroencryptproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FescaroEncryptProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
