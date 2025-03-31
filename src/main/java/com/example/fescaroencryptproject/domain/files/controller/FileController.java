924d018d183909bcca6c59d706ae643ee81c3b78
Reddyong <d0209hong@naver.com>
2025-03-31 15:03:59 +0900

    #5 feat : 암호화 된 파일 다운로드 기능 컨트롤러 로직 기본 틀 작성.

--- src/main/java/com/example/fescaroencryptproject/domain/files/controller/FileController.java
+++ src/main/java/com/example/fescaroencryptproject/domain/files/controller/FileController.java
@@ -6,10 +6,7 @@ import com.example.fescaroencryptproject.domain.files.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -26,6 +23,15 @@ public class FileController {
 
         return ResponseEntity.ok(
                 ApiResponse.ok(FileEncryptResponse.from(fileService.upload(uploadFile)))
+        );
+    }
+
+    @GetMapping(path = "/{file-id}/download-encrypted")
+    public ResponseEntity<ApiResponse<?>> downloadEncrypted(
+            @PathVariable(name = "file-id") Long fileId
+    ) {
+        return ResponseEntity.ok(
+                ApiResponse.ok()
         );
     }
 }
