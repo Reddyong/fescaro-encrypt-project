2abfa42c51c8f7feccfa95654a854a6b77ccb6c4
Reddyong <d0209hong@naver.com>
2025-03-31 23:50:14 +0900

    #11 feat : 암호화 이력 전체 조회 기능 db 조회 로직 구현.

--- src/main/java/com/example/fescaroencryptproject/domain/files/repository/FileRepositoryPort.java
+++ src/main/java/com/example/fescaroencryptproject/domain/files/repository/FileRepositoryPort.java
@@ -1,9 +1,13 @@
 package com.example.fescaroencryptproject.domain.files.repository;
 
 import com.example.fescaroencryptproject.domain.files.entity.File;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface FileRepositoryPort {
     File save(File file);
 
     File findById(Long id);
+
+    Page<File> findAll(Pageable pageable);
 }
