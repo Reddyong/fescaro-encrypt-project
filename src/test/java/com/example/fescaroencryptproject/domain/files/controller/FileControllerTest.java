6cd09f3dd6bd79c9ab03b24bbe0102fe4d2555e0
Reddyong <d0209hong@naver.com>
2025-04-01 20:58:24 +0900

    #17 refactor : 기존 테스트 방식에서 WebMvcTest 방식으로 변경

--- src/test/java/com/example/fescaroencryptproject/domain/files/controller/FileControllerTest.java
+++ src/test/java/com/example/fescaroencryptproject/domain/files/controller/FileControllerTest.java
@@ -8,14 +8,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.*;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,13 +30,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = FileController.class)
+@MockBean(JpaMetamodelMappingContext.class)
 @ActiveProfiles(profiles = "test")
 @DisplayName(value = "파일 컨트롤러 테스트")
 public class FileControllerTest {
 
-    @MockitoBean
+    @MockBean
     private FileService fileService;
 
     @Autowired
