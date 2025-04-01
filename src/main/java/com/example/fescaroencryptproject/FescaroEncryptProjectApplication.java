09d36ec04db874f597df318fb467ea42de2f77e6
Reddyong <d0209hong@naver.com>
2025-04-01 20:57:56 +0900

    #17 feat : EntityScan 추가

--- src/main/java/com/example/fescaroencryptproject/FescaroEncryptProjectApplication.java
+++ src/main/java/com/example/fescaroencryptproject/FescaroEncryptProjectApplication.java
@@ -2,9 +2,11 @@ package com.example.fescaroencryptproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.fescaroencryptproject.domain")
 @EnableJpaAuditing
 public class FescaroEncryptProjectApplication {
 
